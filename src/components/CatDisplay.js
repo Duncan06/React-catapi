3634a8079d5e2acb390be6df24984d84b2fd6244
Duncan06 <tylerdownie0@gmail.com>
2021-09-30 12:15:08 -0700

    Add code to be converted from vanilla to react.

--- src/components/CatDisplay.js
+++ src/components/CatDisplay.js
@@ -1,6 +1,68 @@
 import classes from "./CatDisplay.module.css";
 
 function CatDisplay(props) {
+  function decrement() {
+    count--;
+    images.src = CatGallery[count].url;
+    next.disabled = false;
+    if (count == 0) {
+      this.disabled = true;
+    } else {
+      this.disabled = false;
+    }
+    updateIndex();
+    catInfo();
+  }
+
+  function increment() {
+    count++;
+    images.src = CatGallery[count].url;
+
+    if (count != 0) {
+      previous.disabled = false;
+    }
+
+    if (count == CatGallery.length - 1) {
+      this.disabled = true;
+    } else {
+      this.disabled = false;
+    }
+    updateIndex();
+    catInfo();
+  }
+
+  function updateIndex() {
+    document.querySelector(".count").innerHTML = `<p> ${count + 1} / ${
+      CatGallery.length
+    } </p>`;
+  }
+
+  function catInfo() {
+    if (
+      CatGallery[count].breeds.length != 0 &&
+      "alt_names" in CatGallery[count].breeds[0] &&
+      CatGallery[count].breeds[0].alt_names != ""
+    ) {
+      document.querySelector(".catInfo").innerHTML = `
+           <p> Breed: ${CatGallery[count].breeds[0].name} </p>
+           <p> Alternative name: ${CatGallery[count].breeds[0].alt_names} </p>`;
+      document.querySelector(".description").innerHTML = `
+          <p> Description: ${CatGallery[count].breeds[0].description} </p>`;
+    } else if (CatGallery[count].breeds.length != 0) {
+      document.querySelector(".catInfo").innerHTML = `
+           <p> Breed: ${CatGallery[count].breeds[0].name} </p>
+           <p> Alternative name: Unavailable </p>`;
+      document.querySelector(".description").innerHTML = `
+          <p> Description: ${CatGallery[count].breeds[0].description} </p>`;
+    } else {
+      document.querySelector(".catInfo").innerHTML = `
+           <p> Breed: Unavailable </p>
+           <p> Alternative name: Unavailable </p>`;
+      document.querySelector(".description").innerHTML = `
+          <p> Description: Unavailable </p>`;
+    }
+  }
+
   return (
     <div clasName={classes.wrapper}>
       <div className={classes.ImageDisplay}>
@@ -14,9 +76,9 @@ function CatDisplay(props) {
       <div className={classes.count}></div>
 
       <div className={classes.buttons}>
-        <button disabled>Previous</button>
+        <button disabled onClick={decrement}>Previous</button>
         <button>Random</button>
-        <button disabled>Next</button>
+        <button disabled onClick={increment}>Next</button>
       </div>
     </div>
   );
