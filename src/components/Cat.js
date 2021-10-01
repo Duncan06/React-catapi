c4cfdc71a04c385bba5d3c07a888ae335888eee3
Duncan06 <tylerdownie0@gmail.com>
2021-10-01 14:20:44 -0700

    Add attribute accessors to Cat

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -4,12 +4,17 @@ function Cat(props) {
   return (
     <div clasName={classes.wrapper}>
       <div className={classes.ImageDisplay}>
-        <img className={classes.img} />
+        <img className={classes.img} src={props.image}/>
       </div>
 
-      <div className={classes.catInfo}></div>
+      <div className={classes.catInfo}>
+          <p> {props.breed} </p>
+          <p> {props.alt_names} </p>
+      </div>
 
-      <div className={classes.description}></div>
+      <div className={classes.description}>
+          <p> {props.description} </p>
+      </div>
 
       <div className={classes.count}></div>
 
