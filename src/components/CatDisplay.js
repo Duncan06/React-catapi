8d22752ac60dcd0825ea871b17c180098ceec555
Duncan06 <tylerdownie0@gmail.com>
2021-10-02 13:19:17 -0700

    Remove vanilla javascript, add random.

--- src/components/CatDisplay.js
+++ src/components/CatDisplay.js
@@ -2,6 +2,23 @@ import Cat from "./Cat";
 import classes from "./CatDisplay.module.css";
 
 function CatDisplay(props) {
+  function random() {
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        for (const key in data) {
+          const cat = {
+            id: key,
+            ...data[key],
+          };
+
+          catGallery.push(cat);
+        }
+      });
+  }
+
   function decrement() {
     count--;
     images.src = CatGallery[count].url;
@@ -33,9 +50,11 @@ function CatDisplay(props) {
   }
 
   function updateIndex() {
-    document.querySelector(".count").innerHTML = `<p> ${count + 1} / ${
-      CatGallery.length
-    } </p>`;
+    return (
+      <p>
+        ${count + 1} / ${CatGallery.length}
+      </p>
+    );
   }
 
   function catInfo() {
@@ -44,36 +63,42 @@ function CatDisplay(props) {
       "alt_names" in CatGallery[count].breeds[0] &&
       CatGallery[count].breeds[0].alt_names != ""
     ) {
-      document.querySelector(".catInfo").innerHTML = `
-           <p> Breed: ${CatGallery[count].breeds[0].name} </p>
-           <p> Alternative name: ${CatGallery[count].breeds[0].alt_names} </p>`;
-      document.querySelector(".description").innerHTML = `
-          <p> Description: ${CatGallery[count].breeds[0].description} </p>`;
+      <div>
+        <p> Breed: ${CatGallery[count].breeds[0].name} </p>
+        <p> Alternative name: ${CatGallery[count].breeds[0].alt_names} </p>
+        <p> Description: ${CatGallery[count].breeds[0].description} </p>
+      </div>;
     } else if (CatGallery[count].breeds.length != 0) {
-      document.querySelector(".catInfo").innerHTML = `
-           <p> Breed: ${CatGallery[count].breeds[0].name} </p>
-           <p> Alternative name: Unavailable </p>`;
-      document.querySelector(".description").innerHTML = `
-          <p> Description: ${CatGallery[count].breeds[0].description} </p>`;
+      <div>
+        <p> Breed: ${CatGallery[count].breeds[0].name} </p>
+        <p> Alternative name: Unavailable </p>
+        <p> Description: ${CatGallery[count].breeds[0].description} </p>
+      </div>;
     } else {
-      document.querySelector(".catInfo").innerHTML = `
-           <p> Breed: Unavailable </p>
-           <p> Alternative name: Unavailable </p>`;
-      document.querySelector(".description").innerHTML = `
-          <p> Description: Unavailable </p>`;
+      <div>
+        <p> Breed: Unavailable </p>
+        <p> Alternative name: Unavailable </p>
+        <p> Description: Unavailable </p>
+      </div>;
     }
   }
 
-  return <div>
-    {props.catGallery.map(cat =>
-      <Cat 
-        key={cat.id}
-        id={cat.id}
-        image={cat.image}
-        breed={cat.breed}
-        alt_names={cat.alt_names}
-        description={cat.description} />)}
-  </div>
+  return (
+    <div>
+      {props.catGallery.map((cat) => (
+        <Cat
+          key={cat.id}
+          id={cat.id}
+          image={cat.image}
+          catInfo={catInfo()}
+          index={updateIndex()}
+          OnIncrement={increment}
+          OnDecrement={decrement}
+          OnRandom={random}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default CatDisplay;
